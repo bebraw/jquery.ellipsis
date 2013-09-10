1f60d19f44383f1c21fa977d97aadb66c7bbf6d8
Juho Vepsalainen <bebraw@gmail.com>
2013-09-10 14:02:49 +0300

    Move demo JS to a separate file

---
+++ js/demo.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('#interactive').qdemo({
+        pluginName: 'ellipsis',
+        parentElement: '<p>john eliza mike roger</p>'
+    });
+    $('#bulletedList').ellipsis();
+    $('#orderedList').ellipsis({visible: 2, more: 'more'});
+    $('#definitionList').ellipsis({showCb: function($e) {
+        $e.fadeIn(500);
+    }});
+    $('#paragraph').ellipsis({visible: 10});
+    $('#animatedParagraph').ellipsis({visible: 10, showCb: function($e, t) {
+        $e.hide().text(t).slideDown('slow');
+    }});
+    $('#chars').ellipsis({atFront: true, separator: '', visible: 10});
+});
